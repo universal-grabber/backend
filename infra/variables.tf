fdbffb2422663bcfc9646aa125d4a722ba28f705
Taleh Ibrahimli <talehsmail@gmail.com>
2021-04-10 03:12:16 +0400

    migrate ugb repositories to single place

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,6 +3,6 @@ variable "DOCKER_IMG_TAG" {}
 locals {
   repository = "hub.tisserv.net"
 
-  base_name_api     = "api"
-  base_name_storage = "storage"
+  base_name_api     = "ugb-api"
+  base_name_storage = "ugb-storage"
 }
