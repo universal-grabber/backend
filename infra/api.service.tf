0af7d4ccfbc3ea564106bfe50b67ff78b6968214
Taleh Ibrahimli <talehsmail@gmail.com>
2021-04-11 02:34:28 +0400

    minor fixes

--- infra/api.service.tf
+++ infra/api.service.tf
@@ -16,12 +16,14 @@ resource "kubernetes_service" "ugb-api" {
     port {
       name = "http"
       port = 80
+      node_port = 30003
       target_port = 8080
     }
 
     port {
       name = "grpc"
       port = 6565
+      node_port = 30004
       target_port = 6565
     }
   }
