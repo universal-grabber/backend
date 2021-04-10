849656060cb1d5398695682a692143adb87e464a
Taleh Ibrahimli <talehsmail@gmail.com>
2021-04-11 01:56:51 +0400

    general fixes

--- proto.sh
+++ proto.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-#go install google.golang.org/protobuf/cmd/protoc-gen-go
+go install google.golang.org/protobuf/cmd/protoc-gen-go
+go get google.golang.org/protobuf/cmd/protoc-gen-go
+go get google.golang.org/grpc/cmd/protoc-gen-go-grpc
 
 
 SRC_DIR="proto"
