6d571038cad75150af995a9fa051db2eaeccc2c2
Taleh Ibrahimli <talehsmail@gmail.com>
2021-04-12 04:33:18 +0400

    minor fixes

---
+++ cli/api-client-new.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	pb "backend/gen/proto/service"
+	"backend/processor/model"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+)
+
+type ApiClient struct {
+	pageServiceClient pb.PageRefServiceClient
+	connection        *grpc.ClientConn
+}
+
+func (client *ApiClient) Init(config model.Config) {
+	// initialize grpc
+	// Set up a connection to the server.
+	conn, err := grpc.Dial("10.0.1.77:30004", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	client.connection = conn
+
+	client.pageServiceClient = pb.NewPageRefServiceClient(conn)
+}
