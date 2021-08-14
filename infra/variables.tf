7989eafc7199bc523fb8f34e01e9a2233a858c02
Taleh Ibrahimli <talehsmail@gmail.com>
2021-08-14 14:26:01 +0400

    minor fixes

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,7 +3,6 @@ variable "DOCKER_IMG_TAG" {}
 locals {
   repository = "hub.kube.tisserv.net"
 
-  base_name_api = "ugb-api"
   base_name_storage = "ugb-storage"
   base_name_processor = "ugb-processor"
   base_name_model-parser = "ugb-model-parser"
