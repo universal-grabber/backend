4119118a64e7856c10cd87cf7df366b81f00907b
Taleh Ibrahimli <talehsmail@gmail.com>
2021-08-20 01:12:36 +0400

    minor fixes

--- common/gray-log.go
+++ common/gray-log.go
@@ -9,13 +9,11 @@ func EnableGrayLog(service string) {
 	log.SetReportCaller(true)
 	log.SetLevel(log.TraceLevel)
 
-	log.TraceLevel.String()
-
 	hook := graylog.NewAsyncGraylogHook("ug.tisserv.net:12201", map[string]interface{}{"service": service})
 	hook.Level = log.TraceLevel
 	hook.Writer().CompressionType = 0
 	hook.Writer().CompressionLevel = 9
 
 	log.AddHook(hook)
-	//log.SetFormatter(new(NullFormatter)) // Don't send logs to stdout
+	log.SetFormatter(new(NullFormatter)) // Don't send logs to stdout
 }
